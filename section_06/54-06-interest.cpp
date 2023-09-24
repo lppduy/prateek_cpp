15f529104b5507b349498a69383ea51a312bbf82
lppduy <lephamphuongduy@gmail.com>
2023-09-24 12:34:45 +0700

    v54

---
+++ section_06/54-06-interest.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// int main()
+// {
+//   float p,r,t;
+        
+//   cin >> p >> r >> t;
+        
+//   cout << floor((p*r*t)/100);
+
+//   return 0;
+// }
+
+
+int main()
+{
+  float p,r,t;
+        
+  cin >> p >> r >> t;
+  
+  float simpleInterest = (p*r*t) / 100;
+
+  cout << simpleInterest;
+  
+  return 0;
+}
