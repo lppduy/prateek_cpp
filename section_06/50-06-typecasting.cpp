9d3a700189e266614b8ab40100b49dfe9c0eae91
lppduy <lephamphuongduy@gmail.com>
2023-09-24 11:27:07 +0700

    v50

---
+++ section_06/50-06-typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int a,b,c,d,e;
+  cin >> a >> b >> c >> d >> e;
+  int sum = a + b +c +d + e;git
+
+  cout << fixed << setprecision(4) << (float) sum / 5 << endl;
+ 
+  return 0;
+}
