9f235082e6cf9221c5ba0ce5a73a6101041af2a1
lppduy <lephamphuongduy@gmail.com>
2023-09-25 13:51:59 +0700

    v87

---
+++ section_11/87-default.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <cmath>
+// #include <iomanip>
+using namespace std;
+
+void playMusic(int songId=1,int songId2=6){
+  cout<<"play music...🎹 "<<songId<<" and "<< songId2<<endl;
+}
+
+
+
+int main()
+{
+  playMusic();
+  playMusic(5,11);
+
+  return 0;
+}
